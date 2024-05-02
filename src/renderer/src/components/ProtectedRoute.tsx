2f7dbd08cb2f210f73365eb2afd503559eabd766
trunicz <romelelicg_19@alu.uabcs.mx>
2024-05-02 08:58:51 -0700

    implement useAuthStore in ProtectedRoute

--- src/renderer/src/components/ProtectedRoute.tsx
+++ src/renderer/src/components/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
+import { useAuthStore } from '@renderer/stores/useAuth'
 import { ReactNode } from 'react'
 import { useLocation } from 'wouter'
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }): ReactNode => {
   const [location, setLocation] = useLocation()
 
-  setLocation('/login')
+  const { user } = useAuthStore()
 
-  if (!location.includes('/')) {
+  if (user) {
     setLocation('/app')
+  } else {
+    setLocation('/login')
   }
+  console.log(location, user)
+
   return children
 }
