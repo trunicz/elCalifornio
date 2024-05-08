2e923b09cf0d6df876400b1abc714bd6828ac3f8
trunicz <romelelicg_19@alu.uabcs.mx>
2024-05-07 17:43:12 -0700

    Create GetItem in Inventory

--- src/renderer/src/hooks/useInventory.tsx
+++ src/renderer/src/hooks/useInventory.tsx
@@ -4,10 +4,32 @@ import { useState } from 'react'
 interface InventoryMethods {
   inventory: object[] | null
   getAllInventory: () => Promise<object[] | null>
+  getItem: (id: string | number) => Promise<object | null>
 }
 
 export const useInventory = (): InventoryMethods => {
   const [inventory, setInventory] = useState<object[] | null>(null)
+
+  const getItem = async (id: string | number): Promise<object | null> => {
+    try {
+      const { data, error } = await supabase.from('equipment').select().eq('id', id)
+      if (error) throw error
+      const filteredInventory = data.map((inv) => {
+        return {
+          id: inv.id,
+          nombre: inv.name,
+          cantidad: inv.amount,
+          disponible: inv.available,
+          descripción: inv.description
+        }
+      })
+      setInventory(filteredInventory)
+      return filteredInventory
+    } catch (error) {
+      console.log(error)
+    }
+    return null
+  }
 
   const getAllInventory = async (): Promise<object[] | null> => {
     try {
@@ -31,5 +53,5 @@ export const useInventory = (): InventoryMethods => {
     return null
   }
 
-  return { getAllInventory, inventory }
+  return { getAllInventory, inventory, getItem }
 }
--- src/renderer/src/pages/Inventory/InventoryPage.tsx
+++ src/renderer/src/pages/Inventory/InventoryPage.tsx
@@ -1,11 +1,13 @@
-import { AppLayout, SearchBar, Table } from '@renderer/components'
+import { AppLayout, SearchBar, Table, useModal } from '@renderer/components'
 import { Loading } from '@renderer/components/Loading'
 import { useInventory } from '@renderer/hooks/useInventory'
 import { ReactElement, useEffect, useState } from 'react'
+import { LuMoreHorizontal } from 'react-icons/lu'
 
 export const InventoryPage = (): ReactElement => {
   const [inventoryList, setInventoryList] = useState<unknown[] | null>(null)
-  const { getAllInventory, inventory } = useInventory()
+  const { getAllInventory, inventory, getItem } = useInventory()
+  const { Modal, openModal } = useModal()
 
   useEffect(() => {
     getAllInventory().then((res) => {
@@ -13,13 +15,47 @@ export const InventoryPage = (): ReactElement => {
       setInventoryList(res)
     })
   }, [])
+
+  const moreInfo = (id: string | number): void => {
+    getItem(id).then((res) => {
+      if (res) {
+        const item = res[0]
+        openModal(
+          <>
+            {Object.keys(item)
+              .filter((k) => !['id'].includes(k))
+              .map((key, index) => (
+                <p key={key + index} className="flex gap-2 text-lg">
+                  <span className="w-1/3 text-start">{key.toUpperCase()}:</span>
+                  {item[key] ? (
+                    <span className=" flex-1 bg-gray-100 p-1 px-4 text-start rounded-lg">
+                      {item[key]}
+                    </span>
+                  ) : (
+                    <span className="flex items-center">
+                      <LuMoreHorizontal />
+                    </span>
+                  )}
+                </p>
+              ))}
+          </>
+        )
+      }
+    })
+  }
+
   return (
     <AppLayout>
       <AppLayout.Content>
         <AppLayout.PageOptions pageTitle="Inventario">
           <SearchBar searchFunction={setInventoryList} data={inventory} />
         </AppLayout.PageOptions>
-        {inventoryList ? <Table data={inventoryList} /> : <Loading />}
+        <Modal title="Inventario" className="w-[500px]" />
+        {inventoryList ? (
+          <Table data={inventoryList} hiddenKeys={['id']} watchFunction={moreInfo} />
+        ) : (
+          <Loading />
+        )}
       </AppLayout.Content>
     </AppLayout>
   )
