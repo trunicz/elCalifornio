fa7669fbfa1f48753f537f3daa979cfafc33a43e
trunicz <romelelicg_19@alu.uabcs.mx>
2024-05-09 00:52:56 -0700

    Feat: Create funtion get historial of rentals

--- src/renderer/src/hooks/useRentals.tsx
+++ src/renderer/src/hooks/useRentals.tsx
@@ -8,6 +8,7 @@ interface RentalsMethods {
   deleteRental: (id: string | number) => Promise<void>
   createRental: (values: any) => Promise<void>
   getRental: (id: string | number) => Promise<object | null>
+  getRentalHistory: () => Promise<any[] | null>
 }
 
 export const useRentals = (): RentalsMethods => {
@@ -126,5 +127,50 @@ export const useRentals = (): RentalsMethods => {
     return null
   }
 
-  return { getAllRentals, rentals, deleteRental, createRental, getRental }
+  const getRentalHistory = async (): Promise<any[] | null> => {
+    try {
+      const response = await supabase
+        .from('rentals')
+        .select(
+          'id,clients!rentals_client_id_fkey(name,last_name,id),user_id,end_date,equipment(type(type_name),reference)'
+        )
+        .not('deleted_at', 'is', null)
+
+      const rentals = response.data
+
+      if (response.error) {
+        throw response.error
+      }
+
+      if (rentals) {
+        const filteredRentalsPromises = rentals.map(async (rental) => {
+          const response = await supabase.auth.admin.getUserById(rental.user_id)
+          const user = response.data.user?.user_metadata
+          if (user) {
+            return {
+              id: rental.id,
+              client_id: rental.clients?.id,
+              cliente: `${rental.clients?.name} ${rental.clients?.last_name}`,
+              user_id: rental.user_id,
+              fecha_final: new Date(rental.end_date).toLocaleDateString(),
+              usuario: `${user.name} ${user.lastname}`,
+              alquilado: rental.equipment.map(
+                (item: any) => `${item.type.type_name}: ${item.reference}`
+              )
+            }
+          } else {
+            return null
+          }
+        })
+        const filteredRentals = await Promise.all(filteredRentalsPromises)
+        setRentals(filteredRentals)
+        return filteredRentals
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    return null
+  }
+
+  return { getAllRentals, rentals, deleteRental, createRental, getRental, getRentalHistory }
 }
