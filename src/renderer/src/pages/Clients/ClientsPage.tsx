b09164b9d1eab3e539df31589d86a9f8c68be5ce
Trunicz <romelin2001@hotmail.com>
2024-05-11 14:27:41 -0700

    implement ask before delete in clients

--- src/renderer/src/pages/Clients/ClientsPage.tsx
+++ src/renderer/src/pages/Clients/ClientsPage.tsx
@@ -2,6 +2,7 @@ import { AppLayout, Button, SearchBar, Table, useModal } from '@renderer/compone
 import { Loading } from '@renderer/components/Loading'
 import { useClients } from '@renderer/hooks'
 import { ReactElement, useEffect, useState } from 'react'
+import { IoWarning } from 'react-icons/io5'
 import { LuCheckCircle2 } from 'react-icons/lu'
 import { useLocation } from 'wouter'
 
@@ -64,19 +65,52 @@ export const ClientsPage = (): ReactElement => {
     setLocation('/clients/' + id)
   }
 
-  const deleteFunction = async (id: string | number): Promise<void> => {
-    await deleteClient(id).then(async () => {
-      await getAllClients().then((res) => setClients(res))
-      openModal(
-        <div>
-          <span className="animate-fade-up text-6xl mb-4 flex justify-center text-green-500">
-            <LuCheckCircle2 />
-          </span>
-          <h3>¡El cliente se elimino con éxito!</h3>
-          <Button className="mt-4" color="success" text="Aceptar" onClick={() => closeModal()} />
-        </div>
-      )
+  const remove = async (id: string | number): Promise<void> => {
+    setClients(null)
+    closeModal().then(async () => {
+      await deleteClient(id).then(async () => {
+        await getAllClients().then((res) => setClients(res))
+        openModal(
+          <div>
+            <span className="animate-fade-up text-6xl mb-4 flex justify-center text-green-500">
+              <LuCheckCircle2 />
+            </span>
+            <h3>¡El cliente se elimino con éxito!</h3>
+            <Button className="mt-4" color="success" text="Aceptar" onClick={() => closeModal()} />
+          </div>
+        )
+      })
     })
+  }
+
+  const deleteFunction = async (id: string | number): Promise<void> => {
+    openModal(
+      <>
+        <div className="flex flex-col gap-4">
+          <div className="flex-1 animate-jump flex justify-center items-center text-9xl text-amber-500">
+            <IoWarning />
+          </div>
+          <div className="">
+            <p>Al realizar esta acción no se podrá deshacer</p>
+            <p>¿Quiere continuar?</p>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              className="animate-fade animate-ease-out animate-duration-200"
+              color="danger"
+              text="cancelar"
+              onClick={closeModal}
+            />
+            <Button
+              className="animate-fade animate-ease-out animate-duration-200"
+              color="success"
+              text="aceptar"
+              onClick={() => remove(id)}
+            />
+          </div>
+        </div>
+      </>
+    )
   }
   return (
     <AppLayout>
@@ -85,7 +119,7 @@ export const ClientsPage = (): ReactElement => {
           <SearchBar searchFunction={setClients} data={clientList} />
         </AppLayout.PageOptions>
         <Modal title="cliente" className="w-auto min-w-[450px]" />
-        {clientList ? (
+        {clients ? (
           <Table
             data={clients}
             watchFunction={watchFunction}
