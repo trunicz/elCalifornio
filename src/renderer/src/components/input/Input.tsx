dde71e0e5aa7f6509736a22fd97d88e660d52fdf
trunicz <romelelicg_19@alu.uabcs.mx>
2024-05-16 11:03:46 -0700

    add btn watch option

--- src/renderer/src/components/input/Input.tsx
+++ src/renderer/src/components/input/Input.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { cn } from '@renderer/utils'
-import { ForwardRefRenderFunction, MutableRefObject, forwardRef } from 'react'
+import { ForwardRefRenderFunction, MutableRefObject, forwardRef, useState } from 'react'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
 interface InputProps {
   as: 'input' | 'select' | 'textarea'
@@ -15,17 +16,30 @@ const Input: ForwardRefRenderFunction<
   HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement,
   InputProps
 > = ({ as, type, options = [], value, className, placeholder, ...props }: InputProps, ref) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
   switch (as) {
     case 'input':
       return (
-        <input
-          ref={ref as MutableRefObject<HTMLInputElement>}
-          placeholder={placeholder}
-          className={cn('', className)}
-          value={value as string}
-          type={type}
-          {...props}
-        />
+        <div className="relative overflow-hidden w-full">
+          <input
+            ref={ref as MutableRefObject<HTMLInputElement>}
+            placeholder={placeholder}
+            className={cn('', className)}
+            value={value as string}
+            type={type === 'password' && showPassword ? 'text' : type}
+            {...props}
+          />
+          {type === 'password' && (
+            <button
+              type="button"
+              className="absolute right-0 hover:bg-gray-200 transition-all active:scale-95 rounded-lg border bg-gray-100 p-3"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FiEyeOff /> : <FiEye />}
+            </button>
+          )}
+        </div>
       )
     case 'select':
       return (
