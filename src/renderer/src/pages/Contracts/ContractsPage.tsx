0cdd397361cac157c6c6ed3e038ea842f3c402ca
trunicz <romelelicg_19@alu.uabcs.mx>
2024-05-25 03:04:50 -0700

    Update for use Contracts

--- src/renderer/src/pages/Contracts/ContractsPage.tsx
+++ src/renderer/src/pages/Contracts/ContractsPage.tsx
@@ -1,11 +1,22 @@
-import { AppLayout } from '@renderer/components'
+import { AppLayout, Button } from '@renderer/components'
 import { ReactElement } from 'react'
+import { LuFileBadge } from 'react-icons/lu'
 
 export const ContractsPage = (): ReactElement => {
   return (
     <AppLayout>
       <AppLayout.Content>
-        <h1>Tabla contratos</h1>
+        <div className="flex-1 flex justify-center items-center">
+          <a href="/src/renderer/src/assets/frame.png" download={'frame.png'}>
+            <Button
+              className="w-auto gap-2"
+              color="success"
+              text="Crear Contrato"
+              iconLeft={true}
+              icon={<LuFileBadge />}
+            />
+          </a>
+        </div>
       </AppLayout.Content>
     </AppLayout>
   )
